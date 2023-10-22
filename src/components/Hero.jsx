4107b92dbd16f4fd07e190979defa15449f0c35c
amey09 <ameymhatre09@gmail.com>
2023-10-22 20:54:31 +0530

    feat: add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import { Flex, Heading } from '@chakra-ui/react';
+import React from 'react';
+
+function Hero() {
+  return (
+    <Flex
+      flexDir={'column'}
+      alignItems={'center'}
+      gap={'1rem'}
+      height={'60vh'}
+      justifyContent={'center'}
+      flex={1}
+      whiteSpace={"nowrap"}
+    >
+      <Heading
+        animation="slideLeft 8s cubic-bezier(0.48, 0.48, 0.48, 0.48) infinite alternate"
+        bgClip={'text'}
+        size={'h2'}
+      >
+        UI UX DESIGNER
+      </Heading>
+      <Heading
+        animation="slideRight 8s cubic-bezier(0.48, 0.48, 0.48, 0.48) infinite alternate"
+        bgClip={'text'}
+        size={'h2'}
+      >
+        PINAK GHOGALE
+      </Heading>
+    </Flex>
+  );
+}
+
+export default Hero;
